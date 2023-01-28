2b66c56526d6e810ff76a639f374de2f763b99cf
boom <ranjit.makwana@gmail.com>
2023-01-28 23:35:12 +0000

    set up day schedule events in index.html and arrays for jquery to use, plus get saved scheduled events from local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,21 @@
 
-$("#currentDate").text(moment().format("dddd, MMMM Do"));
+    var daySchedule = ["9am","10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+    var scheduledEvents = [];
+    $("#currentDate").text(moment().format("dddd, MMMM Do"));
+    var currentHour = moment().hour();
+
+    if (!getDaysEvents()) {
+        scheduledEvents = ["","","","","","","","",""];
+    }
+
+    function getDaysEvents() {
+        var daysEvents = localStorage.getItem("scheduledEvents");
+        console.log("days events ", daysEvents);
+        if (daysEvents) {
+            scheduledEvents = JSON.parse(daysEvents);
+            console.log(scheduledEvents);
+            return true;
+        }
+        return false;
+    }
 
