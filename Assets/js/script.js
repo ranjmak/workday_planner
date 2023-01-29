14112bd95e7c29b9741614a541af8b5c03423228
boom <ranjit.makwana@gmail.com>
2023-01-29 15:44:52 +0000

    changed logic to only save the current event, means that if an event is input but not saved, it will not show on the next load from local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,10 +10,10 @@ $(document).ready(function() {
 
     function getDaysEvents() {
         var daysEvents = localStorage.getItem("scheduledEvents");
-        console.log("days events ", daysEvents);
+        //console.log("days events ", daysEvents);
         if (daysEvents) {
             scheduledEvents = JSON.parse(daysEvents);
-            console.log(scheduledEvents);
+            //console.log(scheduledEvents);
             return true;
         }
         return false;
@@ -22,7 +22,7 @@ $(document).ready(function() {
     for(var i = 0; i < daySchedule.length; i++) { 
         $("#"+i+"a").append("<div class='col-md-1' id='scheduleTime'><p>"+daySchedule[i]+"</p></div>"+
                         "<form class='col-md-10' id='formSchedule"+i+"'><div class='form-group'><textarea class='col-md-10 description scheduleText' rows='3' id='textSchedule"+i+"'></textarea></div></form>"+
-                        "<button class='col-md-1 saveBtn' id='eventSave"+i+"'><i class='fa fa-save' style='font-size:24px'></i></button>");
+                        "<button class='col-md-1 saveBtn' id='eventSave"+i+"'><i class='fa fa-save' data-index='"+i+"' style='font-size:24px'></i></button>");
 
         if(i+9 < currentHour) {
             $("#textSchedule"+i).addClass("past 'form-control' required");
@@ -38,10 +38,19 @@ $(document).ready(function() {
         $("#eventSave"+i).on("click", eventSave);
 
     }
-    function eventSave() {
+    function eventSave(event) {
+        var index = event.target.getAttribute("data-index");
+        console.log(index);
+
+        scheduledEvents.splice(index, 1, $("#textSchedule"+index).val());
+
+/*
         for (var i = 0; i < scheduledEvents.length; i++) {
             scheduledEvents[i] = $("#textSchedule"+i).val();
         }
+*/
+
+//console.log(scheduledEvents);
         localStorage.setItem("scheduledEvents", JSON.stringify(scheduledEvents));
 
         $("#msgSaved").append("<div class='col-md-12 msgSaved'><p>The scheduled event has been saved <i class='fa fa-check' aria-hidden='true'></i></p></div>");
